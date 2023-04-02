557b37f7ef86127ce6bf924b3170a5881d806f57
Kimber Ogden <mx.kimber@gmail.com>
2023-04-02 12:28:41 -0500

    fixed errors in controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
 
   def index
-    @songs = Songs.all
+    @songs = Song.all
     render :index
   end
 
@@ -24,7 +24,7 @@ class SongsController < ApplicationController
     @song = Song.find_by(id: params[:id])
     @song.title = params[:title] || @song.title
     @song.artist = params[:artist] || @song.artist
-    @song.album = params[:album] || || @song.album
+    @song.album = params[:album] || @song.album
     @song.year = params[:year] || @song.year
 
     @song.save
@@ -32,7 +32,7 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    @song = song.find_by(id: params[:id})
+    @song = song.find_by(id: params[:id])
     @song.destroy
     render json: {message: "song destroyed sucessfully"}
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,5 @@ Rails.application.routes.draw do
   get "/songs/:id" => "songs#destroy"
 
 end
+
+gem "rack-cors"
